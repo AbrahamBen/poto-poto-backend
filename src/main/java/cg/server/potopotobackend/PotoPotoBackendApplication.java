537eeb42cdfb246bffdff628bd4153106c2f9d3c
AbrahamBen <eternelle.vision@outlook.fr>
2020-06-20 02:54:15 +0100

    initial commit

---
+++ src/main/java/cg/server/potopotobackend/PotoPotoBackendApplication.java
@@ -0,0 +1,13 @@
+package cg.server.potopotobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotoPotoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotoPotoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cg/server/potopotobackend/PotoPotoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cg.server.potopotobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotoPotoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
